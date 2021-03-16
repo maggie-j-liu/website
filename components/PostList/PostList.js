519c2d81e84b2a432075b82fbef2f6673423f361
maggie-j-liu <maggie.j.liu@gmail.com>
2021-03-16 16:53:08 -0700

    smooth scroll to posts

--- components/PostList/PostList.js
+++ components/PostList/PostList.js
@@ -21,35 +21,35 @@ const formatDate = (date) => {
     return formattedDate;
 }
 
-export default class PostList extends React.Component {
-    render() {
-        return (
-            <div id={'posts'} className={'min-w-full px-32 py-12 flex flex-1 flex-col justify-center items-center bg-home-main-light bg-gradient-to-b dark:bg-home-gray-900'}>
-                <span className={'text-2xl font-semibold uppercase pt-5 -mt-3 text-home-primary-600 dark:text-home-primary-500'}>
-                    Posts
-                </span>
-                <div className={'grid grid-cols-2 lg:grid-cols-3 overflow-x-auto py-4 gap-8'}>
-                    {this.props.posts.map((post) => (
-                        <Link
-                            as={`/${postsDir}/${post.filePath.replace(/\.mdx?$/, '')}`}
-                            href={`/${postsDir}/[slug]`}
-                            key={post.filePath}
-                        >
-                            <a className={'group block px-4 py-5 bg-home-primary-50 dark:bg-home-gray-800 rounded-md shadow-md'}>
-                                <p className={'text-sm text-gray-400 font-semibold'}>
-                                    {post.data.date ? formatDate(post.data.date) : <br />}
-                                </p>
-                                <p className={'font-semibold text-home-primary-600 dark:text-home-primary-400 group-hover:text-home-contrast-600 group-hover:underline dark:group-hover:text-home-contrast-300'}>
-                                    {post.data.title}
-                                </p>
-                                <p className={'text-gray-600 dark:text-white'} style={{overflow: 'hidden', display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical'}}>
-                                    {post.data.preview}
-                                </p>
-                            </a>
-                        </Link>
-                    ))}
-                </div>
+const PostList = React.forwardRef((props, ref) => {
+    return (
+        <div id={'posts'} ref={ref} className={'min-w-full px-32 py-12 flex flex-1 flex-col justify-center items-center bg-home-main-light bg-gradient-to-b dark:bg-home-gray-900'}>
+            <span className={'text-2xl font-semibold uppercase pt-5 -mt-3 text-home-primary-600 dark:text-home-primary-500'}>
+                Posts
+            </span>
+            <div className={'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 overflow-x-auto py-4 gap-8'}>
+                {props.posts.map((post) => (
+                    <Link
+                        as={`/${postsDir}/${post.filePath.replace(/\.mdx?$/, '')}`}
+                        href={`/${postsDir}/[slug]`}
+                        key={post.filePath}
+                    >
+                        <a className={'group block px-4 py-5 bg-home-primary-50 dark:bg-home-gray-800 rounded-md shadow-md'}>
+                            <p className={'text-sm text-gray-400 font-semibold'}>
+                                {post.data.date ? formatDate(post.data.date) : <br />}
+                            </p>
+                            <p className={'font-semibold text-home-primary-600 dark:text-home-primary-400 group-hover:text-home-contrast-600 group-hover:underline dark:group-hover:text-home-contrast-300'}>
+                                {post.data.title}
+                            </p>
+                            <p className={'text-gray-600 dark:text-white'} style={{overflow: 'hidden', display: '-webkit-box', WebkitLineClamp: 3, WebkitBoxOrient: 'vertical'}}>
+                                {post.data.preview}
+                            </p>
+                        </a>
+                    </Link>
+                ))}
             </div>
-        );
-    };
-}
+        </div>
+    );
+});
+
+export default PostList;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
@@ -8,6 +9,7 @@ import PostList from '../components/PostList/PostList';
 import { POSTS_PATH, postFilePaths } from '../utils/posts';
 
 export default function Home({posts}) {
+    const postsRef = React.useRef();
     return (
         <div>
             <Head>
@@ -16,18 +18,25 @@ export default function Home({posts}) {
             </Head>
             <NavBar page='home'/>
             <div className={'bg-gradient-to-b from-home-secondary-100 to-home-contrast-50 dark:from-home-secondary-900 dark:to-home-contrast-900 min-w-full min-h-screen p-20 flex flex-1 flex-col justify-center items-center'}>
-                <h1 className={'text-8xl font-bold bg-gradient-to-br from-home-primary-700 to-home-contrast-300 dark:from-home-primary-400 dark:to-home-contrast-300 text-transparent bg-clip-text mb-8'}>
+                <h1 className={'text-6xl sm:text-7xl md:text-8xl font-bold bg-gradient-to-br from-home-primary-700 to-home-contrast-300 dark:from-home-primary-400 dark:to-home-contrast-300 text-transparent bg-clip-text mb-8'}>
                     Hello, world!
                 </h1>
-                <Link href={'#posts'}>
-                    <a>
-                        <svg className="text-home-primary-400 dark:text-home-primary-200 w-12 h-12 relative top-40 animate-bounce" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <Link 
+                    href={'#posts'}
+                >
+                    <a
+                        onClick={e => {
+                            e.preventDefault();
+                            postsRef.current.scrollIntoView({ behavior: 'smooth' });
+                        }}
+                    >
+                        <svg className="text-home-primary-400 dark:text-home-primary-200 w-12 h-12 absolute top-3/4 animate-bounce" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
                         </svg>
                     </a>
                 </Link>
             </div>
-            <PostList posts={posts}/>
+            <PostList posts={posts} ref={postsRef}/>
         </div>
     );
 }
