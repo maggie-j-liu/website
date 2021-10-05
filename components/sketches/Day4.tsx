b0b21f42444b55dae3b658ca75c5a5996ec19e91
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-04 23:25:24 -0700

    knot

---
+++ components/sketches/Day4.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+const Knot = () => {
+  let [path, setPath] = React.useState("M 100, 100");
+  let [altPath, setAltPath] = React.useState("M 100, 100");
+  const [showAltPath, setShowAltPath] = React.useState(false);
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  React.useEffect(() => {
+    const numPoints = 50 + Math.random() * 50;
+    for (let i = 0; i < numPoints; i++) {
+      const x = Math.random() * 200;
+      const _x = x - 1 + Math.random() * 2;
+      const y = Math.random() * 200;
+      const _y = y - 1 + Math.random() * 2;
+      const x2 = Math.random() * 200;
+      const _x2 = x2 - 1 + Math.random() * 2;
+      const y2 = Math.random() * 200;
+      const _y2 = y2 - 1 + Math.random() * 2;
+      path += ` S ${x2}, ${y2} ${x}, ${y}`;
+      altPath += ` S ${_x2}, ${_y2} ${_x}, ${_y}`;
+    }
+    setPath(path);
+    setAltPath(altPath);
+  }, []);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setShowAltPath(!showAltPath);
+    }, 400);
+  }, [showAltPath]);
+  if (!mounted) {
+    return (
+      <div className={"w-4/5 mx-auto"}>
+        <svg viewBox="0 0 200 200"></svg>
+      </div>
+    );
+  }
+  return (
+    <div className={"w-4/5 mx-auto"}>
+      <svg viewBox="0 0 200 200">
+        {showAltPath ? (
+          <path
+            d={altPath}
+            strokeWidth="2px"
+            fill="transparent"
+            className="stroke-current"
+          />
+        ) : (
+          <path
+            d={path}
+            strokeWidth="2px"
+            fill="transparent"
+            className="stroke-current"
+          />
+        )}
+      </svg>
+    </div>
+  );
+};
+
+export default Knot;
