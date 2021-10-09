23d5c1cf8eb387ae7ba2b4905022d13b367d5dce
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-08 21:48:37 -0700

    watch

---
+++ components/sketches/Day8.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+const Watch = () => {
+  const eyesRef = React.useRef(null);
+  const [bounds, setBounds] = React.useState(null);
+  const [rotate, setRotate] = React.useState(0);
+  React.useEffect(() => {
+    const boundingRect = eyesRef.current.getBoundingClientRect();
+    setBounds({
+      x: boundingRect.x + boundingRect.width / 2,
+      y: boundingRect.y + boundingRect.height / 2,
+    });
+  }, [eyesRef]);
+
+  React.useEffect(() => {
+    if (!bounds) return;
+    return window.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      const dx = clientX - bounds.x;
+      const dy = -(clientY - bounds.y);
+      let theta = Math.atan(dy / dx);
+      if (dx < 0) {
+        theta += Math.PI;
+      }
+      setRotate(theta);
+    });
+  }, [bounds]);
+  return (
+    <div className="w-4/5 mx-auto">
+      <svg
+        ref={eyesRef}
+        viewBox="0 0 200 200"
+        style={{ transformBox: "fill-box" }}
+      >
+        <circle
+          cx={60}
+          cy={100}
+          r={40}
+          fill="transparent"
+          className={"stroke-current stroke-2"}
+        />
+        <circle
+          cx={140}
+          cy={100}
+          r={40}
+          fill="transparent"
+          className={"stroke-current stroke-2"}
+        />
+        <circle
+          cx={60}
+          cy={80}
+          r={20}
+          className={"stroke-current fill-current stroke-2 origin-bottom"}
+          style={{
+            transformBox: "fill-box",
+            transform: `rotate(calc(90deg + ${-rotate}rad))`,
+          }}
+        />
+        <circle
+          cx={140}
+          cy={80}
+          r={20}
+          className={"stroke-current fill-current stroke-2 origin-bottom"}
+          style={{
+            transformBox: "fill-box",
+            transform: `rotate(calc(90deg + ${-rotate}rad))`,
+          }}
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Watch;
