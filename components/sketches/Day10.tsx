f910505e05fd400c5e17163b16d673e65930852f
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-10 23:18:04 -0700

    pick

---
+++ components/sketches/Day10.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+const Pick = () => {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted)
+    return (
+      <div className="w-4/5 mx-auto">
+        <svg viewBox="0 0 200 200"></svg>
+      </div>
+    );
+  const picked = Math.floor(Math.random() * 16);
+  return (
+    <div className="w-4/5 mx-auto">
+      <svg viewBox="0 0 200 200">
+        {[...Array(16)].map((_, idx) => {
+          let j = idx % 4;
+          let i = (idx - j) / 4;
+          return (
+            <circle
+              key={idx}
+              r={25}
+              cx={50 * j + 25}
+              cy={50 * i + 25}
+              className={`fill-current ${
+                picked === idx
+                  ? "text-contrast-500"
+                  : "text-primary-500 dark:text-primary-400"
+              }`}
+            />
+          );
+        })}
+      </svg>
+    </div>
+  );
+};
+
+export default Pick;
--- components/sketches/Day9.tsx
+++ components/sketches/Day9.tsx
@@ -3,24 +3,16 @@ const Pressure = () => {
   return (
     <div className="w-4/5 mx-auto">
       <svg viewBox="0 0 200 200">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="200"
-          height="200"
-          fill="none"
-          viewBox="0 0 200 200"
-        >
-          <g>
-            <path
-              d="M103 208.5l85.5-80.5-81-19-4.5 99.5zM92.5 212.5l8-168.5L36 31l56.5 181.5z"
-              className="fill-current text-[#324D66] dark:text-gray-400"
-            />
-            <path
-              d="M156 212l70-185-111 25.5L156 212zM35 224L62 90l-68 13.059L35 224z"
-              className="fill-current text-gray-900 dark:text-white"
-            />
-          </g>
-        </svg>
+        <g>
+          <path
+            d="M103 208.5l85.5-80.5-81-19-4.5 99.5zM92.5 212.5l8-168.5L36 31l56.5 181.5z"
+            className="fill-current text-[#324D66] dark:text-gray-400"
+          />
+          <path
+            d="M156 212l70-185-111 25.5L156 212zM35 224L62 90l-68 13.059L35 224z"
+            className="fill-current text-gray-900 dark:text-white"
+          />
+        </g>
       </svg>
     </div>
   );
