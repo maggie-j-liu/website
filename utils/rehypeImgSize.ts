d4a6b3d75f135ccc0367ff5bd16a8f55a1fe3737
maggie-j-liu <maggie.j.liu@gmail.com>
2022-02-13 17:50:21 -0800

    disable video sizing for now

--- utils/rehypeImgSize.ts
+++ utils/rehypeImgSize.ts
@@ -1,7 +1,8 @@
 // https://github.com/ksoichiro/rehype-img-size/blob/master/index.js
 import visit from "unist-util-visit";
 import imageSize from "image-size";
-import videoSize from "get-video-dimensions";
+// import ffprobe from "ffprobe";
+// import ffprobeStatic from "ffprobe-static";
 import path from "path";
 
 // https://github.com/syntax-tree/unist-util-visit-parents/issues/8#issuecomment-619381050
@@ -24,11 +25,14 @@ const getImageSize = (src) => {
   return imageSize(src);
 };
 
-const getVideoSize = async (src) => {
-  src = path.join(dir, src);
-  return await videoSize(src);
-};
-
+// const getVideoSize = async (src) => {
+//   src = path.join(dir, src);
+//   const info = await ffprobe(src, { path: ffprobeStatic.path });
+//   return {
+//     width: info.streams[0].width,
+//     height: info.streams[0].height,
+//   };
+// };
 const setImageSize = (options) => {
   dir = options?.dir;
   return transform;
@@ -63,20 +67,20 @@ const onelement = async (node) => {
         let dims;
         if (imgImport.source.endsWith(".mp4")) {
           node.name = "video";
-          dims = await getVideoSize(imgImport.source);
+          //dims = await getVideoSize(imgImport.source);
         } else {
           dims = getImageSize(imgImport.source);
+          node.attributes.push({
+            type: "mdxJsxAttribute",
+            name: "width",
+            value: dims.width,
+          });
+          node.attributes.push({
+            type: "mdxJsxAttribute",
+            name: "height",
+            value: dims.height,
+          });
         }
-        node.attributes.push({
-          type: "mdxJsxAttribute",
-          name: "width",
-          value: dims.width,
-        });
-        node.attributes.push({
-          type: "mdxJsxAttribute",
-          name: "height",
-          value: dims.height,
-        });
       }
     }
   }
