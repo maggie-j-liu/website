02f7172850afb77b05d5a4ba828a9f7daeea8baf
maggie-j-liu <maggie.j.liu@gmail.com>
2022-03-21 12:15:46 -0700

    rss feed

---
+++ scripts/rss.ts
@@ -0,0 +1,40 @@
+// based on https://github.com/kentcdodds/kentcdodds.com/blob/main/app/routes/blog.rss%5B.%5Dxml.tsx
+import { writeFileSync } from "fs";
+import { getSortedPosts } from "@/lib/getPosts";
+import path from "path";
+
+const BASE_URL = "https://maggieliu.dev";
+const posts = getSortedPosts();
+const cdata = (s: string) => `<![CDATA[${s}]]>`;
+
+const rssFeed = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Maggie's Website</title>
+    <link>${BASE_URL}</link>
+    <description>Maggie Liu's website</description>
+    <language>en-us</language>
+    <atom:link href="${BASE_URL}/rss.xml" rel="self" type="application/rss+xml"/>
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+    ${posts
+      .map((post) =>
+        `
+    <item>
+      <title>${cdata(post.data.title)}</title>
+      <description>${
+        post.data.preview ? cdata(post.data.preview) : ""
+      }</description>
+      <pubDate>${new Date(post.data.date).toISOString().split("T")[0]}</pubDate>
+      <link>${BASE_URL}/${post.slug}</link>
+      <guid>${BASE_URL}/${post.slug}</guid>
+      ${(post.data.tags ?? [])
+        .map((tag: string) => `<category>${cdata(tag)}</category>`)
+        .join("\n      ")}
+    </item>
+    `.trim()
+      )
+      .join("\n    ")}
+  </channel>
+</rss>`;
+
+writeFileSync(path.join(process.cwd(), "public", "rss.xml"), rssFeed);
